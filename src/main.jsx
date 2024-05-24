722933978fdfc968e1ecad23584e3790a009a1b2
Speakiez <aerholgocalin57@gmail.com>
2024-05-24 21:33:12 +0800

    Add scene, camera, and renderer in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#background")
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
