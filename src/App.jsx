cd055592d5f40ab8381440033b14d841fc480a1f
Speakiez <aerholgocalin57@gmail.com>
2024-05-25 21:16:36 +0800

    Add cloud img with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-
+import cloudImg from './assets/vaporwaveCloud.png'
 export default function App() {
 
   return (
     <>
+      <img className="left cloud" src={cloudImg} alt="a cloud"/>
+      <img className="right cloud" src={cloudImg} alt="a cloud"/>
       <h1>Drum Machine</h1>
     </>
   )
