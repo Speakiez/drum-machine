13b85820e446d585c6502f8570db54aed10f48f4
Speakiez <aerholgocalin57@gmail.com>
2024-05-25 20:55:17 +0800

    Style page with more vaporwave

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import * as THREE from 'three'
+import './index.css'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#background")
+const renderer = new THREE.WebGLRenderer({ 
+  canvas: document.querySelector("#background"),
+  alpha: true
 });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ( 30 );
 
-renderer.render(scene, camera);
+renderer.render( scene, camera );
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xC65BCF, wireframe: true });
-const torus = new THREE.Mesh(geometry, material);
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry( 15, 32, 16 ),
+  new THREE.MeshBasicMaterial({ 
+    color: 0xF6F969, 
+    wireframe: true 
+  })
+);
 
-scene.add(torus);
+scene.add(sphere);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  sphere.rotation.x += 0.005;
+  sphere.rotation.y += 0.001;
+  sphere.rotation.z += 0.005;
+
 
   renderer.render(scene, camera);
 }
