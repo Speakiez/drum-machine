2b218f6917c5d13b5c975a17243cf8b9c47e10f4
Speakiez <aerholgocalin57@gmail.com>
2024-05-28 09:11:28 +0800

    Refactor to move all elements inside the root elm

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,81 @@
+import { useEffect } from 'react'
+import * as THREE from 'three'
 import cloudImg from './assets/vaporwaveCloud.png'
-import gridImg from './assets/vaporwaveGrid.png'
+import grid from './assets/myGrid.jpg'
 
 export default function App() {
+  useEffect(() => {
+    const canvas = document.getElementById("background");
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ 
+      canvas: document.querySelector("#background"),
+      alpha: true
+    });
 
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.position.setZ( 30 );
+
+    renderer.render( scene, camera );
+
+    const gridTexture = new THREE.TextureLoader().load( grid );
+
+    const sphere = new THREE.Mesh(
+      new THREE.SphereGeometry( 15, 32, 16 ),
+      new THREE.MeshBasicMaterial({ 
+        color: 0xF6F969, 
+        wireframe: true 
+      })
+    );
+
+    sphere.position.setY(3);
+
+    const plane = new THREE.Mesh(
+      new THREE.PlaneGeometry( 130, 25 ),
+      new THREE.MeshBasicMaterial({
+        map: gridTexture
+      })
+    );
+
+    plane.rotateX(-45);
+    plane.position.setY(-20);
+
+    scene.add( sphere );
+    scene.add( plane );
+
+    function animate() {
+      requestAnimationFrame( animate );
+
+      sphere.rotation.x += 0.005;
+      sphere.rotation.y += 0.001;
+      sphere.rotation.z += 0.005;
+
+
+      renderer.render( scene, camera );
+    }
+
+    animate();
+
+    window.addEventListener("resize", () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize( window.innerWidth, window.innerHeight );
+    });
+  });
+  
   return (
     <>
-      <img className="cloud left" src={cloudImg} alt="a cloud"/>
-      <img className="cloud right" src={cloudImg} alt="a cloud"/>
-      <h1>Drum Machine</h1>
+      <canvas id="background"></canvas>
+      <div className="background-imgs">
+        <img className="cloud left" src={cloudImg} alt="a cloud"/>
+        <img className="cloud right" src={cloudImg} alt="a cloud"/>
+      </div>
+      <div id="title">Drum Machine</div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,71 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import * as THREE from 'three'
 import './index.css'
-import grid from './assets/myGrid.jpg'
 
-const canvas = document.getElementById("background");
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ 
-  canvas: document.querySelector("#background"),
-  alpha: true
-});
-
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-camera.position.setZ( 30 );
-
-renderer.render( scene, camera );
-
-const gridTexture = new THREE.TextureLoader().load(grid);
-
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry( 15, 32, 16 ),
-  new THREE.MeshBasicMaterial({ 
-    color: 0xF6F969, 
-    wireframe: true 
-  })
-);
-
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry( 130, 25 ),
-  new THREE.MeshBasicMaterial({
-    map: gridTexture
-  })
-);
-
-plane.rotateX(-45);
-plane.position.setY(-20);
-
-scene.add(sphere);
-scene.add(plane);
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  sphere.rotation.x += 0.005;
-  sphere.rotation.y += 0.001;
-  sphere.rotation.z += 0.005;
-
-
-  renderer.render(scene, camera);
-}
-
-animate();
-
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-});
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('drum-machine')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
